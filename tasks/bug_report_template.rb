d6783b570b5051fabc68e97eb6dfa1744e41831a
David Rodríguez <deivid.rodriguez@riseup.net>
2018-12-31 11:51:31 +0100

    Explicitly depend on sassc-rails

--- tasks/bug_report_template.rb
+++ tasks/bug_report_template.rb
@@ -13,7 +13,6 @@ gemfile(true) do
   # Change Rails version if necessary.
   gem 'rails', '5.2.1.1'
 
-  gem 'sassc-rails', '2.0.0'
   gem 'sqlite3', '1.3.13', platform: :mri
   gem 'activerecord-jdbcsqlite3-adapter', "52.0", platform: :jruby
   gem 'jruby-openssl', '0.10.1', platform: :jruby
